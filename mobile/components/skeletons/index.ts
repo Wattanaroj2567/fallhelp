9f42f848c58f81a2dcafa2a80adf29f73c4ecb34
Wattanaroj Butdee <159878664+Wattanaroj2567@users.noreply.github.com>
2025-12-19 01:59:44 +0700

    Delete mobile directory

--- mobile/components/skeletons/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Skeleton Loaders
- * Animated placeholder components for better loading UX
- */
-
-export { ListItemSkeleton } from './ListItemSkeleton';
-export { CardSkeleton } from './CardSkeleton';
-export { ProfileSkeleton } from './ProfileSkeleton';
